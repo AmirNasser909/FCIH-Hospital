00c2c736feca3b614046bf17320b17b3a7d68cac
AmirNasser909 <121165112+AmirNasser909@users.noreply.github.com>
2023-09-26 19:41:49 +0300

    Add files via upload

---
+++ adib.js
@@ -0,0 +1,8 @@
+function color(){
+document.getElementById("add").style.color="red";
+}
+function color4(){
+    
+document.getElementById("add").style.color="blue";
+    
+}
---
+++ contact.js
@@ -0,0 +1,39 @@
+var department =  prompt("whats the department you need it ?")
+switch(department)
+{
+    case "nervous system":
+        alert("this department already exists for us");
+        break;
+    
+    case "dentistry":
+        alert("this department already exists for us");
+        break;
+    
+    case "Diagnostic Radiology":
+        alert("this department already exists for us");
+        break;
+    
+    case "Anesthesia and Pain Management":
+        alert("this department already exists for us");
+        break;
+    
+   case "Evolutionary Pediatrics":
+        alert("this department already exists for us");
+        break;
+    
+     case "Gastrointestinal unit and binoculars":
+        alert("this department already exists for us");
+        break;
+    
+     case "Public emergency":
+        alert("this department already exists for us");
+        break;
+    
+    case "plastic surgery":
+        alert("this department already exists for us");
+        break;
+    
+    default:
+        alert("this department not exists for us");
+        break;
+}
